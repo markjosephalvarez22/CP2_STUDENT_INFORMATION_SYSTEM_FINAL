5a174ed8f46f17fe8498a976cc82c0bcd3947c42
Mark Joseph Alvarez <markjosephalvarez875@gmail.com>
2026-06-12 22:04:15 +0800

    Implemented table creation and add student function

--- student_model.py
+++ student_model.py
@@ -7,3 +7,29 @@ class StudentModel:
 
         self.conn = sqlite3.connect("database/students.db")
         self.cursor = self.conn.cursor()
+
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS students(
+            student_id TEXT PRIMARY KEY,
+            name TEXT,
+            age INTEGER,
+            course TEXT,
+            year_level TEXT
+        )
+        """)
+
+        self.conn.commit()
+
+    def add_student(self, student):
+
+        self.cursor.execute("""
+        INSERT INTO students VALUES (?, ?, ?, ?, ?)
+        """, (
+            student["student_id"],
+            student["name"],
+            student["age"],
+            student["course"],
+            student["year_level"]
+        ))
+
+        self.conn.commit()

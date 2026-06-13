8672f275d2cd542a07d6506325d8659c1bbbf467
Mark Joseph Alvarez <markjosephalvarez875@gmail.com>
2026-06-13 09:43:25 +0800

    Added add student method & Implemented student input collection

--- student_controller.py
+++ student_controller.py
@@ -5,3 +5,13 @@ class StudentController:
     def __init__(self):
         self.model = StudentModel()
         self.view = StudentView()
+    def add_student(self):
+
+        student = {
+            "student_id": input("ID: "),
+            "name": input("Name: "),
+            "age": int(input("Age: ")),
+            "course": input("Course: "),
+            "year_level": input("Year Level: ")
+        }
+

49cbb9ad268cf6797662a96ec0eeaeb9ea4c63ea
Ashley Basila <ashibasila8@gmail.com>
2026-06-13 09:34:38 +0800

    Created Student View structure and menu display

--- student_view.py
+++ student_view.py
@@ -1,1 +1,17 @@
+class StudentView:
+
+    @staticmethod
+    def display_menu():
+
+        print("\n==============================")
+        print(" STUDENT INFORMATION SYSTEM")
+        print("==============================")
+
+        print("1. Add Student")
+        print("2. View Students")
+        print("3. Search Student")
+        print("4. Update Student")
+        print("5. Delete Student")
+        print("6. Count Students")
+        print("7. Exit")
 

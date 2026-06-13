d6fbf802d002eaa4feaaca4d413618eeb757be96
Mark Joseph Alvarez <markjosephalvarez875@gmail.com>
2026-06-13 09:41:12 +0800

    Created Student Controller class structure

--- student_controller.py
+++ student_controller.py
@@ -1,1 +1,7 @@
+from student_model import StudentModel
+from student_view import StudentView
 
+class StudentController:
+    def __init__(self):
+        self.model = StudentModel()
+        self.view = StudentView()

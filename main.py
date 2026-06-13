11a5a7ac41a906a8ada1e20febb5932a0d6893a2
Mark Joseph Alvarez <markjosephalvarez875@gmail.com>
2026-06-13 12:08:49 +0800

    Connected menu options to controller methods

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ while True:
     controller.view.display_menu()
 
     choice = input("Choice: ")
+
+    if choice == "1":
+        controller.add_student()
+
+    elif choice == "2":
+        controller.view_students()
+
+    elif choice == "3":
+        controller.search_student()
+
+    elif choice == "4":
+        controller.update_student()
+
+    elif choice == "5":
+        controller.delete_student()
+
+    elif choice == "6":
+        controller.count_students()
